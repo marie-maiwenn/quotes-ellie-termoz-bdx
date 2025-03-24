2335dfb73616bc08e5f6c2e67514f82f4a378c94
jules <julestermoz@gmail.com>
2025-03-24 16:36:12 +0100

    Initial quotes program

---
+++ quotes.c
@@ -0,0 +1,14 @@
+//
+// Created by jules on 24/03/2025.
+//
+
+#include "quotes.h"
+
+void print_quote(char ** quotes, int index) {
+    printf("%s\n", quotes[index]);
+}
+
+void print_random_quote(char ** quotes) {
+    int index = rand() % MAX_QUOTES;
+    print_quote(quotes, index);
+}
